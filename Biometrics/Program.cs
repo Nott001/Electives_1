f037dd38055b9d1de1b08e0a5587f7f2715538b2
Villamor <113780247+Nott001@users.noreply.github.com>
2026-04-15 20:58:19 +0800

    new

--- Biometrics/Program.cs
+++ Biometrics/Program.cs
@@ -11,7 +11,7 @@ namespace Biometrics
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new employee_attendance_report());
+            Application.Run(new employee_registration());
         }
     }
 }
